473b3f83ce80b7845777e99e1a36ee2fc94f2f6e
endroni <rubao.rcs@gmail.com>
2019-10-07 10:45:01 -0300

    Tela de Login e Cadastro criadas

---
+++ Sistema.View/frmCadUsuario.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Sistema.View
+{
+    partial class frmCadUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.txtNovo = new System.Windows.Forms.Button();
+            this.txtSalvar = new System.Windows.Forms.Button();
+            this.txtExcluir = new System.Windows.Forms.Button();
+            this.txtEditar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Usuário:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Senha:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Enabled = false;
+            this.txtNome.Location = new System.Drawing.Point(81, 33);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(231, 20);
+            this.txtNome.TabIndex = 3;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Enabled = false;
+            this.txtUsuario.Location = new System.Drawing.Point(81, 66);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(231, 20);
+            this.txtUsuario.TabIndex = 4;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Enabled = false;
+            this.txtSenha.Location = new System.Drawing.Point(81, 101);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(231, 20);
+            this.txtSenha.TabIndex = 5;
+            // 
+            // txtNovo
+            // 
+            this.txtNovo.Location = new System.Drawing.Point(356, 14);
+            this.txtNovo.Name = "txtNovo";
+            this.txtNovo.Size = new System.Drawing.Size(75, 23);
+            this.txtNovo.TabIndex = 6;
+            this.txtNovo.Text = "Novo";
+            this.txtNovo.UseVisualStyleBackColor = true;
+            // 
+            // txtSalvar
+            // 
+            this.txtSalvar.Location = new System.Drawing.Point(356, 43);
+            this.txtSalvar.Name = "txtSalvar";
+            this.txtSalvar.Size = new System.Drawing.Size(75, 23);
+            this.txtSalvar.TabIndex = 7;
+            this.txtSalvar.Text = "Salvar";
+            this.txtSalvar.UseVisualStyleBackColor = true;
+            // 
+            // txtExcluir
+            // 
+            this.txtExcluir.Location = new System.Drawing.Point(356, 72);
+            this.txtExcluir.Name = "txtExcluir";
+            this.txtExcluir.Size = new System.Drawing.Size(75, 23);
+            this.txtExcluir.TabIndex = 8;
+            this.txtExcluir.Text = "Excluir";
+            this.txtExcluir.UseVisualStyleBackColor = true;
+            // 
+            // txtEditar
+            // 
+            this.txtEditar.Location = new System.Drawing.Point(356, 101);
+            this.txtEditar.Name = "txtEditar";
+            this.txtEditar.Size = new System.Drawing.Size(75, 23);
+            this.txtEditar.TabIndex = 9;
+            this.txtEditar.Text = "Editar";
+            this.txtEditar.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(30, 141);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(401, 101);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // frmCadUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 245);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtEditar);
+            this.Controls.Add(this.txtExcluir);
+            this.Controls.Add(this.txtSalvar);
+            this.Controls.Add(this.txtNovo);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmCadUsuario";
+            this.Text = "frmCadUsuario";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Button txtNovo;
+        private System.Windows.Forms.Button txtSalvar;
+        private System.Windows.Forms.Button txtExcluir;
+        private System.Windows.Forms.Button txtEditar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
